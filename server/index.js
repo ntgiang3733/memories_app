ee9fb75ec3ec9e1144ce83dffc1bf5464049aa9d
giangnt <ntgiang3733@gmail.com>
2022-01-26 11:53:41 +0700

    server

--- server/index.js
+++ server/index.js
@@ -4,23 +4,22 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-import userRoutes from './routes/user.js';
-
+import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(express.json({ limit: '30mb', extends: true}));
-app.use(express.urlencoded({ limit: '30mb', extends: true}))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb://localhost:27017/memories_app';
-const PORT = 5000;
+const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
